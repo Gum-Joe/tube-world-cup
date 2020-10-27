5f9366d2d76deb108125276438c7b5719ad29606
Kishan <kishansambhi@hotmail.co.uk>
2020-10-27 18:44:09 +0000

    Add upcoming quarter finals

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,16 @@ const classes: { [key: string]: string } = {
   "Central": "central",
   "District": "district",
   "Circle": "circle",
-  "Thameslink": "thameslink"
+  "Thameslink": "thameslink",
+  "H&C": "handc",
+  "Victoria": "victoria",
+  "Metropolitan": "metropolitan",
+  "Trams": "trams",
+  "Overground": "overground",
+  "CableCar": "cablecar",
+  "DLR": "dlr",
+  "Piccadilly": "picadilly",
+  "???": "unknown",
 }
 
 const colours: { [key: string]: string } = {
@@ -27,7 +36,15 @@ const colours: { [key: string]: string } = {
   "Central": "rgb(220, 36, 31)",
   "District": "rgb(0, 125, 50)",
   "Circle": "rgb(255, 211, 41)",
-  "Thameslink": "#E9438D"
+  "Thameslink": "#E9438D",
+  "H&C": "rgb(244, 169, 190);",
+  "Victoria": "rgb(0, 152, 216);",
+  "Metropolitan": "rgb(155, 0, 88)",
+  "Trams": "rgb(0, 189, 25)",
+  "Overground": "rgb(239, 123, 16)",
+  "CableCar": "rgb(220, 36, 31)",
+  "DLR": "rgb(0, 175, 173)",
+  "Piccadilly": "rgb(0, 25, 168)"
 }
 
 const DEVNULL = "https://devnull-as-a-service.com/dev/null";
@@ -57,7 +74,55 @@ const pairs = [
     "Thameslink",
     PREFIX + "/api/Tweet/GetTweet?url=https://twitter.com/geofftech/status/1321002110801108993",
     "true",
+  ],
+  [
+    "H&C",
+    "Victoria",
+    DEVNULL,
+  ],
+  [
+    "Metropolitan",
+    "Trams",
+    DEVNULL,
+  ],
+  [
+    "Overground",
+    "CableCar",
+    DEVNULL,
+  ],
+  [
+    "DLR",
+    "Piccadilly",
+    DEVNULL
   ]
+]
+
+const quarterfinals = [
+  [
+    "Northern",
+    "Jubilee",
+    DEVNULL,
+    "false",
+    "Waterloo"
+  ],
+  [
+    "???",
+    "???",
+    DEVNULL,
+    "false",
+  ],
+  [
+    "???",
+    "???",
+    DEVNULL,
+    "false",
+  ],
+  [
+    "???",
+    "???",
+    DEVNULL,
+    "false",
+  ],
 ]
 
 interface StateInfo {
@@ -74,6 +139,7 @@ interface StateInfo {
   winner: number;
   link: string;
   today: boolean;
+  venue?: string;
 }
 
 class CustomizedLabel extends Component<{ [key:string]: any }> {
@@ -91,13 +157,14 @@ class CustomizedLabel extends Component<{ [key:string]: any }> {
   }
 };
 
-class App extends Component<any, { results: StateInfo[] }> {
+class App extends Component<any, { results: StateInfo[], resultsQFinals: StateInfo[] }> {
 
   constructor(props: any) {
     super(props);
 
     this.state = {
-      results: []
+      results: [],
+      resultsQFinals: [],
     }
   }
 
@@ -107,56 +174,65 @@ class App extends Component<any, { results: StateInfo[] }> {
     setInterval(() => bound(), 20000);
   }
 
-  updateResults() {
-    const newPairs: Promise<StateInfo>[] = pairs.map(async (tuple): Promise<StateInfo> => {
-      let one: number = 0;
-      let two: number = 0;
-      let winner: number = 0;
-      try {
-        if (tuple[2] !== DEVNULL) {
-          // Can get it
-          const res = await fetch(tuple[2]);
+  async getUpdates(tuple: string[]): Promise<StateInfo> {
+    let one: number = 0;
+    let two: number = 0;
+    let winner: number = 0;
+    try {
+      if (tuple[2] !== DEVNULL) {
+        // Can get it
+        const res = await fetch(tuple[2]);
 
-          const resBody = await res.text();
-          // Assume pairs in correct order
-          const splitted = resBody.split("\n");
-          const oneHere = splitted[0].match(/^\d+|\d+\b|\d+(?=\w)/g) || ["0"];
-          one = parseInt(oneHere[0], 10);
-          if (oneHere.length > 1 && oneHere[1] === "1") {
-            winner = 1;
-          }
-
-          const twoHere = splitted[1].match(/^\d+|\d+\b|\d+(?=\w)/g) || ["0"];
-          two = parseInt(twoHere[0], 10);
-          if (twoHere.length > 1 && twoHere[1] === "1") {
-            winner = 2;
-          }
-
+        const resBody = await res.text();
+        // Assume pairs in correct order
+        const splitted = resBody.split("\n");
+        const oneHere = splitted[0].match(/^\d+|\d+\b|\d+(?=\w)/g) || ["0"];
+        one = parseInt(oneHere[0], 10);
+        if (oneHere.length > 1 && oneHere[1] === "1") {
+          winner = 1;
         }
-      } catch (err) {
-        //console.log(err.stack);
-      } finally {
-        return {
-          one: {
-            name: tuple[0],
-            votes: one,
-            className: classes[tuple[0]]
-          },
-          two: {
-            name: tuple[1],
-            votes: two,
-            className: classes[tuple[1]]
-          },
-          winner,
-          link: tuple[2].split("url=")[1],
-          today: tuple[3] === "true" ? true : false,
+
+        const twoHere = splitted[1].match(/^\d+|\d+\b|\d+(?=\w)/g) || ["0"];
+        two = parseInt(twoHere[0], 10);
+        if (twoHere.length > 1 && twoHere[1] === "1") {
+          winner = 2;
         }
+
       }
-    });
+    } catch (err) {
+      console.log(err.stack);
+    } finally {
+      return {
+        one: {
+          name: tuple[0],
+          votes: one,
+          className: classes[tuple[0]]
+        },
+        two: {
+          name: tuple[1],
+          votes: two,
+          className: classes[tuple[1]]
+        },
+        winner,
+        link: tuple[2].split("url=")[1],
+        today: tuple[3] === "true" ? true : false,
+        venue: tuple[4],
+      }
+    }
+  }
+
+  updateResults() {
+    const newPairs: Promise<StateInfo>[] = pairs.map(this.getUpdates);
 
     Promise.all(newPairs).then((results) => this.setState({
       results,
     }));
+
+    const newquarterFinals: Promise<StateInfo>[] = quarterfinals.map(this.getUpdates);
+
+    Promise.all(newquarterFinals).then((resultsQFinals) => this.setState({
+      resultsQFinals,
+    }))
   }
 
   render() {
@@ -273,11 +349,49 @@ class App extends Component<any, { results: StateInfo[] }> {
           </Row>
         </Container>
 
+        <h3>Upcoming quarterfinals:</h3>
+        <Table striped bordered responsive>
+          <thead>
+            <tr>
+              <th>Venue</th>
+              <th>Line 1</th>
+              <th>Votes</th>
+              <th>%</th>
+              <th>Line 2</th>
+              <th>Votes</th>
+              <th>%</th>
+              <th>Link</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.resultsQFinals.map((result) => {
+                return (
+                  <tr>
+                    <td>{result.venue || "???"}</td>
+                    <td className={result.one.className}>{result.one.name} {result.winner === 1 ? <FontAwesomeIcon icon={faCheckCircle} /> : ""}</td>
+                    <td>{result.one.votes}</td>
+                    <td style={{
+                      "backgroundSize": `100% ${((result.one.votes / (result.one.votes + result.two.votes) * 100) || 0).toFixed(1)}%`
+                    }} className={result.one.className}>{((result.one.votes / (result.one.votes + result.two.votes) * 100) || 0).toFixed(1)}%</td>
+                    <td className={result.two.className}>{result.two.name} {result.winner === 2 ? <FontAwesomeIcon icon={faCheckCircle} /> : ""}</td>
+                    <td>{result.two.votes}</td>
+                    <td style={{
+                      "backgroundSize": `100% ${((result.two.votes / (result.one.votes + result.two.votes) * 100) || 0).toFixed(1)}%`
+                    }} className={result.two.className}>{((result.two.votes / (result.one.votes + result.two.votes) * 100) || 0).toFixed(1)}%</td>
+                    <td><a href={result.link}>View</a></td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </Table>
+
         <h3>Past games:</h3>
         <Container>
           <Row>
             {
-              this.state.results.filter(result => !result.today).map((result) => {
+              this.state.results.filter(result => !result.today && typeof result.link !== "undefined").map((result) => {
 
                 return (
                   <Col sm md lg>
